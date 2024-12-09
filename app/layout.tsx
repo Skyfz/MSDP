2d5135e16fe236c1d8a8140d1bf6004a710e538f
Skyfz <mikesap34@gmail.com>
2024-12-09 23:35:26 +0200

    error_fix_commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,10 @@ export const viewport: Viewport = {
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
